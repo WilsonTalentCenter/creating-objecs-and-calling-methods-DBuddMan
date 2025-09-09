375f9add35b87c491371df95d61346589b5f9439
DBuddMan <jacksongbudd@gmail.com>
2025-09-09 13:06:16 -0400

    asd
    asd

---
+++ creating-objects/src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+// create a Date object called currentDate  using the default constructor with no parameters
+const currentDate = new Date();
+        // create a Date object called anotherDate  using the default constructor with no parameters
+const anotherDate = new Date();
+        // call the "toString" method on anotherDate and put it into a string variable called anotherDateString
+const anotherDateString = anotherDate.toString();
+        // call the "setTime" method for anotherDate passing in 123478 as its input.
+
+        // call the "before" method on currentDate passing in anotherDate  put the returned value into a boolean variable called isBefore.
+
+        // print the value of isBefore followed by a newline character
+
+////////////////
+        //create a Scanner object called myScanner,  pass in System.in to as the parameter to the constructor
+
+        //call the "nextLine" method on the myScanner object save the result into a string variable called resString
+
+        //print resString followed by a newline character
+
+        //call the "next" method on the myScanner object saving the result into resString
+
+        //print resString followed by a newline character
+    }
+}
